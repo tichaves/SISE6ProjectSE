7cd69c5567cb7672e2543ed81c39f24497579798
tichaves <tichaves@PT-PC0VRQAZ.atrema.deloitte.com>
2020-03-28 18:47:42 +0000

    interface

--- sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/mbway/MBWayInterface.java
+++ sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/mbway/MBWayInterface.java
@@ -2,8 +2,14 @@ package pt.ulisboa.tecnico.learnjava.sibs.mbway;
 
 import java.util.Scanner;
 
+import pt.ulisboa.tecnico.learnjava.bank.domain.Bank;
+import pt.ulisboa.tecnico.learnjava.bank.domain.Bank.AccountType;
+import pt.ulisboa.tecnico.learnjava.bank.domain.Client;
 import pt.ulisboa.tecnico.learnjava.bank.exceptions.AccountException;
+import pt.ulisboa.tecnico.learnjava.bank.exceptions.BankException;
+import pt.ulisboa.tecnico.learnjava.bank.exceptions.ClientException;
 import pt.ulisboa.tecnico.learnjava.sibs.domain.MBWay;
+import pt.ulisboa.tecnico.learnjava.sibs.domain.MBWayAccount;
 import pt.ulisboa.tecnico.learnjava.sibs.exceptions.OperationException;
 import pt.ulisboa.tecnico.learnjava.sibs.exceptions.SibsException;
 
@@ -43,10 +49,11 @@ public class MBWayInterface {
 //      return user; 
 //  } 
 	
-	public static void main(String[] args) throws NumberFormatException, SibsException, AccountException, OperationException {
+	public static void main(String[] args) throws NumberFormatException, SibsException, AccountException, OperationException, BankException, ClientException {
 		
 		//fetch student record based on his roll no from the database
 //		User model  = retriveUserFromDatabase();
+		retriveDataFromDatabase();
 		MBWayModel model = new MBWayModel(MBWAY);
 
 		//Create a view : to write student details on console
@@ -67,6 +74,27 @@ public class MBWayInterface {
 			controller.updateView(input);
 		}
 		myObj.close();
+	}
+	
+	private static void retriveDataFromDatabase() throws BankException, ClientException, AccountException {
+		Bank bank = new Bank("cgd");
+		Client client1 = new Client(bank, "Bonifacio", "Jacobino", "123456789", "966696669", "Algures perdido", 24);
+		Client client2 = new Client(bank, "Aquilino", "Andarilho", "012345678", "933393336", "Algures procurando", 29);
+		Client client3 = new Client(bank, "Felizmino", "Tristemino", "233223323", "965432198", "Rua d'Ele", 25);
+		String iban1 = bank.createAccount(AccountType.CHECKING, client1, 1000, 1000);
+		String iban2 = bank.createAccount(AccountType.CHECKING, client2, 1000, 1000);
+		String iban3 = bank.createAccount(AccountType.CHECKING, client3, 1000, 1000);
+		MBWayAccount account1 = new MBWayAccount(iban1, bank.getAccountByAccountId(iban1.substring(3)));
+		MBWayAccount account2 = new MBWayAccount(iban2, bank.getAccountByAccountId(iban2.substring(3)));
+		MBWayAccount account3 = new MBWayAccount(iban3, bank.getAccountByAccountId(iban3.substring(3)));
+		account1.setActive();
+		account2.setActive();
+		account3.setActive();
+//		MBWay mbway = new MBWay("962770474", account1);
+//		mbway.addFriend("912345678", account2);
+//		mbway.addFriend("931234567", account3);
+//		return mbway;
+
 	}
 
 }
